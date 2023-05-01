020ead15f5b436b74814869e902863e83d0d45b4
Aman Upadhyay <amazeballsaman@gmail.com>
2023-05-01 15:19:08 +0530

    applied style to the components

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -9,13 +9,33 @@ function App() {
     setNewTask(event.target.value);
   };
 
+  const addTask = () => {
+    const task = {
+      id : todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+    };
+    setTodoList([...todoList, task]);
+  };
+
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) =>  task.id !== id));
+  };
+
   return (
     <div className="App">
       <div className='addTask'>
         <input onChange={handleChange} type="text" />
-        <button>Add Task</button>
+        <button onClick={addTask}>Add Task</button>
       </div>
-      <div className="list">{newTask}</div>
+      
+      <div className="list">
+        {todoList.map((task) => {   
+          return <div className='task'>
+            <h1>{task.taskName}</h1>
+            <button onClick={ () => deleteTask(task.id)} >X</button>
+          </div>
+        })}
+      </div>
 
     </div>
   );
