c1e964fb6cbdfcf8386ea2f34e3c7b5f8802e76d
Aman Upadhyay <amazeballsaman@gmail.com>
2023-05-01 20:48:16 +0530

    modified favicons and name

--- todo-list/src/Task.js
+++ todo-list/src/Task.js
@@ -1,6 +1,6 @@
 export const Task = (props) => {
     return (
-        <div className='task' style={{backgroundColor : props.completed ? "#69ffaf" : "white" , marginBottom: "10px"}}>
+        <div className='task' style={{backgroundColor : props.completed ? "#69ffaf" : "white" , marginBottom: "10px" , borderRadius:"5px"}}>
           <h1>{props.taskName}</h1>
           <button onClick={ () => props.deleteTask(props.id)} >X</button>
           <button onClick={ () => props.completeTask(props.id)}>Complete </button>
